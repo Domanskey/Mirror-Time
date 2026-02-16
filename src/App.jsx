4d20ae2e45986f5abba7b6ea94fc60e39234c386
domanskey <domadam04@gmail.com>
2026-02-16 14:42:10 +0100

    feat: add static version

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useState, useEffect } from "react";
 import "./index.css";
 
-function App() {
-  return <h1>Yolo</h1>;
-}
+export default function App() {
+  const [date, setDate] = useState(new Date());
 
-export default App;
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDate(new Date());
+    }, 100);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const format = (num) => num.toString().padStart(2, "0");
+
+  return (
+    <main>
+      <p className="text-5xl">
+        {format(date.getHours())}:{format(date.getMinutes())}:
+        {format(date.getSeconds())}
+      </p>
+    </main>
+  );
+}
